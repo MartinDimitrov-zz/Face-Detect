7678895e088a55d5b44d30aa90c3f2452e5f3a7a
martin <martin@MacBook-Pro.local>
2020-06-01 09:22:51 +0300

    Updates

---
+++ precache-manifest.9e1511398df477e7a07ed82f30dfff0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8d39ce82123d0460d082dab819644f",
+    "url": "/Face-Detect/index.html"
+  },
+  {
+    "revision": "85c6a913c9ce9b7abe59",
+    "url": "/Face-Detect/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "70a64cb826f2c94789df",
+    "url": "/Face-Detect/static/css/main.923bbafa.chunk.css"
+  },
+  {
+    "revision": "85c6a913c9ce9b7abe59",
+    "url": "/Face-Detect/static/js/2.126f09e7.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Face-Detect/static/js/2.126f09e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70a64cb826f2c94789df",
+    "url": "/Face-Detect/static/js/main.8cf15468.chunk.js"
+  },
+  {
+    "revision": "c0ed89b9c14dd2e310de",
+    "url": "/Face-Detect/static/js/runtime-main.825b7d9c.js"
+  }
+]);
