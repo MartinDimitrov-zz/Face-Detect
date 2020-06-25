14e6097630f2021d189c13cdd60ece7dbc275bec
martin <martin@MacBook-Pro.local>
2020-06-25 12:21:01 +0300

    pwa

--- src/components/FaceDetect/FaceDetect.js
+++ src/components/FaceDetect/FaceDetect.js
@@ -5,8 +5,8 @@ const FaceDetect = ({ imageUrl, box }) => {
     return (
         <div className='center ma'>
             <div className='absolute mt2'>
-                <img id='input-image' alt='' src={imageUrl} width='500px' heigh='auto'/>
-                {box.map((boxitem)=>{ 
+                <img id='input-image' alt='' src={imageUrl} width='500px' heigh='auto'/>                
+                {box.map((boxitem)=>{                     
                     return (<div className="bounding-box"  key={boxitem.id} style={{top:boxitem.topRow, right:boxitem.rightCol, bottom:boxitem.bottomRow, left:boxitem.leftCol}}></div>); })
                 }
             </div>
